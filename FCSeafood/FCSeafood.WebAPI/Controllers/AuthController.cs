72c021dbb9f38facff7544671a84e69a7453898d
Oleh Lovchuk <73631796+olovchuk@users.noreply.github.com>
2023-04-16 12:30:36 +0300

    Added SignInRefresh & SignOut in auth controller. Set cookie only server - false

--- FCSeafood/FCSeafood.WebAPI/Controllers/AuthController.cs
+++ FCSeafood/FCSeafood.WebAPI/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
+using FCSeafood.BLL.User.Auth.Helpers;
 using FCSeafood.BLL.User.Auth.Models.Params;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FCSeafood.WebAPI.Controllers;
 
@@ -14,6 +16,23 @@ public class AuthController : ControllerBase {
     [HttpPost("SignIn")]
     public async Task<IActionResult> SignInAsync(SignInParams singInParams) {
         var result = await _jwtCookieAuthService.SignInAsync(singInParams);
+        if (!result.IsSuccessful) return BadRequest(result);
+
+        return Ok(result);
+    }
+
+    [HttpPost("SignInRefresh")]
+    public async Task<IActionResult> SignInRefreshAsync(SignInRefreshParams SignInRefreshParams) {
+        var result = await _jwtCookieAuthService.SignInRefreshAsync(SignInRefreshParams);
+        if (!result.IsSuccessful) return BadRequest(result);
+
+        return Ok(result);
+    }
+
+    [HttpPost("SignOut")]
+    public IActionResult _SignOut() {
+        var claims = JWTClaimsHelper.GetUserClaims(Request.HttpContext.User.Claims);
+        var result = _jwtCookieAuthService.SignOut(new SignOutParams(claims.UserId));
         if (!result.IsSuccessful) return BadRequest(result);
 
         return Ok(result);
