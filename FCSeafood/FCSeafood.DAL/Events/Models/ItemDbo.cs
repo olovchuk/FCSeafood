23b7b8170123c4be53a285d33e57357dfb46d4ee
Oleh Lovchuk <73631796+olovchuk@users.noreply.github.com>
2023-04-29 10:09:43 +0300

    added default values for item dbo

--- FCSeafood/FCSeafood.DAL/Events/Models/ItemDbo.cs
+++ FCSeafood/FCSeafood.DAL/Events/Models/ItemDbo.cs
@@ -17,13 +17,13 @@ public class ItemDbo {
     public Guid PriceId { get; set; }
 
     [Column("FK_Category_Type")]
-    public CategoryType CategoryType { get; set; }
+    public CategoryType CategoryType { get; set; } = CategoryType.Unknown;
 
     [Column("FK_Sub_Category_Type")]
-    public SubCategoryType SubCategoryType { get; set; }
+    public SubCategoryType SubCategoryType { get; set; } = SubCategoryType.Unknown;
 
     [Column("FK_Item_Status_Type")]
-    public ItemStatusType ItemStatusType { get; set; }
+    public ItemStatusType ItemStatusType { get; set; } = ItemStatusType.Unknown;
 
     [Column("Quantity_Per_Kg")]
     public double QuantityPerKg { get; set; }
