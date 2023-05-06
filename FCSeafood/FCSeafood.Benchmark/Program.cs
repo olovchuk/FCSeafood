1266d8b0c38831466af3771833cd6f404ac7fae7
Oleh Lovchuk <73631796+olovchuk@users.noreply.github.com>
2023-05-06 12:53:05 +0300

    test

--- FCSeafood/FCSeafood.Benchmark/Program.cs
+++ FCSeafood/FCSeafood.Benchmark/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System.Text.Json;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using FCSeafood.BLL.Helpers;
 using FCSeafood.BLL.Item;
 using FCSeafood.BLL.Item.Models.Common;
 using FCSeafood.BLL.Item.Models.Params;
+using FCSeafood.BLL.Services;
 using FCSeafood.BusinessObjects;
 using FCSeafood.DAL.Context;
+using FCSeafood.DAL.Events.Models;
+using FCSeafood.DAL.Events.Repository;
 using Microsoft.EntityFrameworkCore;
 
 BenchmarkRunner.Run<ItemBenchmark>();
@@ -13,20 +17,22 @@ BenchmarkRunner.Run<ItemBenchmark>();
 [MemoryDiagnoser]
 [RankColumn]
 public class ItemBenchmark {
+    private const string ConnectionString = "Data Source=localhost;Initial Catalog=Test;User ID=FCS_User;Password=s1c@r8;TrustServerCertificate=True";
     private readonly ItemManager _itemManager;
 
-    public ItemBenchmark(ItemManager itemManager) {
-        _itemManager = itemManager;
+    public ItemBenchmark() {
+        var dbEventOptions = new DbContextOptionsBuilder<EventFCSeafoodContext>();
+        dbEventOptions.UseSqlServer(ConnectionString);
+        var eventFcSeafoodContext = new EventFCSeafoodContext(dbEventOptions.Options);
+        var itemMapperHelper = new ItemMapperHelper();
+        var itemRepository = new ItemRepository(eventFcSeafoodContext);
+        var itemService = new ItemService(itemMapperHelper, itemRepository);
+        _itemManager = new ItemManager(itemService);
     }
 
+    [Benchmark]
     public async Task GetItemListAsync() {
         var tmp = await _itemManager.GetItemListAsync(new ItemByFilterParams(new ItemFilterModel {
-            CategoryType = CategoryType.Fish, SubCategoryType = SubCategoryType.RiverFish
-        }));
-    }
-    
-    public void GetItemList() {
-        var tmp = _itemManager.GetItemListAsync(new ItemByFilterParams(new ItemFilterModel {
             CategoryType = CategoryType.Fish, SubCategoryType = SubCategoryType.RiverFish
         }));
     }
