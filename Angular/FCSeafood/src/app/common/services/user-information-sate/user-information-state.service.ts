924363b4825218a7b6bea88f1c3244bc5ed78703
Oleh Lovchuk <73631796+olovchuk@users.noreply.github.com>
2023-05-13 10:10:40 +0300

    Removed log

--- Angular/FCSeafood/src/app/common/services/user-information-sate/user-information-state.service.ts
+++ Angular/FCSeafood/src/app/common/services/user-information-sate/user-information-state.service.ts
@@ -2,7 +2,6 @@ import {Injectable} from "@angular/core";
 import {AuthStateService} from "../auth-sate/auth-state.service";
 import {UserInformationData} from "../../data/user-information/user-information.data";
 import {UserInformationState} from "../../states/user-information.state";
-import {UserInformationModel} from "@common-data/user-information/models/common/user-information.model";
 
 
 @Injectable({providedIn: 'root'})
@@ -19,12 +18,10 @@ export class UserInformationStateService {
   async Init(): Promise<UserInformationState> {
     if (this.authStateService.IsAuthorized) {
       const result = await this.userInformationDate.GetUserInformationAsync();
-      console.log("result:", result);
-      console.log("result.userInformation:", result.userInformationModel);
-      console.dir(result.userInformationModel)
       if (result.isSuccessful)
         this.userInformationState.userInformation = result.userInformationModel;
     }
+
     this.userInformationState.ResolveInit(null);
     return this.userInformationState;
   }
