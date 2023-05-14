06274ba99293c4e8a90e38d50802d396fe841c4a
Oleh Lovchuk <73631796+olovchuk@users.noreply.github.com>
2023-05-14 14:57:46 +0300

    Fixed load page and optimized load categories/subcategories.

--- Angular/FCSeafood/src/app/modules/pages/shop/shop-content/subcategory/subcategory.component.ts
+++ Angular/FCSeafood/src/app/modules/pages/shop/shop-content/subcategory/subcategory.component.ts
@@ -1,33 +1,47 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CommonDataStateService} from "@common-services/common-data-state/common-data-state.service";
 import {ActivatedRoute} from "@angular/router";
 import {CategoryType, CategoryTypeValues} from "@common-enums/category.type";
 import {SubcategoryTModel} from "@common-data/common/models/common/subcategory-type.model";
+import {CommonDataState} from "@common-states/common-data.state";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'shop-subcategory',
   templateUrl: './subcategory.component.html',
   styleUrls: ['./subcategory.component.scss']
 })
-export class SubcategoryComponent implements OnInit {
+export class SubcategoryComponent implements OnInit, OnDestroy {
   title: string = '';
   subcategoryTListModel: SubcategoryTModel[] = [];
+  routeSubscription!: Subscription;
 
-  constructor(private commonDataStateService: CommonDataStateService,
+  constructor(private commonDataState: CommonDataState,
               private route: ActivatedRoute) {
   }
 
   async ngOnInit(): Promise<void> {
-    let category = CategoryType.Unknown;
+    new Promise((resolve, reject) => {
+      this.routeSubscription = this.route.params.subscribe(async (params) => {
+        let category = CategoryType.Unknown;
 
-    this.route.params.subscribe(params => {
-      let categoryTypeValue = CategoryTypeValues.find(category => category.value === params['categoryType'].toLowerCase());
-      if (categoryTypeValue)
-        category = categoryTypeValue?.id;
+        this.route.params.subscribe(params => {
+          let categoryTypeValue = CategoryTypeValues.find(category => category.value === params['categoryType'].toLowerCase());
+          if (categoryTypeValue)
+            category = categoryTypeValue?.id;
+        });
+
+        this.title = 'Choose subcategory'
+        this.subcategoryTListModel = this.commonDataState.bindCategoryListModel.filter(x => x.categoryTModel.type === category).map(x => x.subcategoryTModel);
+
+        resolve(null);
+      });
     });
 
-    this.title = 'Choose subcategory'
-    this.subcategoryTListModel = await this.commonDataStateService.getSubcategoryByCategoryListAsync(category);
-    console.log(this.subcategoryTListModel)
+
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubscription.unsubscribe();
   }
 }
